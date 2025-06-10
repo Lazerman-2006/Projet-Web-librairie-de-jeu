b20c73af5cd03511281e44f877ec29b465195339
Matteo <matteo.pierron@etudiant.univ-reims.fr>
2025-06-10 09:36:48 +0200

    Doc Catégorie

--- src/Entity/Categorie.php
+++ src/Entity/Categorie.php
@@ -3,27 +3,47 @@
 declare(strict_types=1);
 
 namespace Entity;
-
+/**
+ * Class catégorie qui représente les catégories des jeux vidéos
+ */
 class Categorie
 {
     private int $id;
     private string $description;
 
+    /**
+     * @return int Retourne l'id de l'objet Catégorie
+     */
     public function getId(): int
     {
         return $this->id;
     }
 
+    /**
+     * Permet de définir un id d'un objet
+     * @param int $id ID que l'on souhaite ajouter
+     * @return void
+     */
     public function setId(int $id): void
     {
         $this->id = $id;
     }
 
+
+    /**
+     * @return string Retourne la description de l'objet Catégorie
+     */
     public function getDescription(): string
     {
         return $this->description;
     }
 
+
+    /**
+     * Permet de définir la description d'un objet
+     * @param string $description
+     * @return void
+     */
     public function setDescription(string $description): void
     {
         $this->description = $description;
