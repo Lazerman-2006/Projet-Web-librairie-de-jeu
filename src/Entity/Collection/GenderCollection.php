0c0b7cd5f480372752d83c58985424a205fad3e3
fago0014 <amelien.fagot-naude@etudiant.univ-reims.fr>
2025-06-10 11:13:23 +0200

    Add CategorieGender(findAllGender) method

--- src/Entity/Collection/GenderCollection.php
+++ src/Entity/Collection/GenderCollection.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Entity\Collection;
 
 use Database\MyPdo;
+use Entity\Categorie;
 use Entity\Gender;
 use PDO;
 
@@ -16,6 +17,8 @@ class GenderCollection
      * @param int $genderId
      * @return array
      */
+
+
     public static function findByGenderId(int $genderId): array
     {
         $gender = MyPdo::getInstance()->prepare(
@@ -28,6 +31,19 @@ class GenderCollection
         $gender->execute(['genderId' => $genderId]);
 
         return $gender->fetchAll(PDO::FETCH_CLASS, Gender::class);
+    }
+
+    public static function findAllCollection(): array
+    {
+        $stmt = MyPdo::getInstance()->prepare(
+            <<<'SQL'
+            SELECT id,description
+            FROM gende
+            SQL
+        );
+        $stmt->execute();
+
+        return $stmt->fetchAll(PDO::FETCH_CLASS, Gender::class);
     }
 
 }
