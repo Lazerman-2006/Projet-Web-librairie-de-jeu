7d6155e474353c85b5ae77038698f75677f36331
Matteo <matteo.pierron@etudiant.univ-reims.fr>
2025-06-10 09:31:32 +0200

    Class Categorie With get and set

---
+++ src/Entity/Categorie.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Entity;
+
+class Categorie
+{
+    private int $id;
+    private string $description;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+
+
+}
