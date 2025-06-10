3343b1c3f32350df9ce0264dc05dbdd79f5dd5d7
Matteo <matteo.pierron@etudiant.univ-reims.fr>
2025-06-10 10:12:18 +0200

    Add class Developer

---
+++ src/Entity/Developer.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Entity;
+/**
+ * Class Developer qui représente les developer des jeux vidéos
+ */
+class Developer
+{
+    private int $id;
+    private string $name;
+
+    /** Permet d'avoir l'id d'un developer
+     * @return int Retourne l'id de l'objet
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Permet de définir un id d'un developer
+     * @param int $id ID que l'on souhaite ajouter
+     * @return void
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+
+    /** Permet d'avoir le nom d'un developer
+     * @return string Retourne le nom de l'objet
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+
+    /**
+     * Permet de définir le nom d'un developer
+     * @param string $name Le nom que l'on souhaite modifié
+     * @return void
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+
+
+
+
+}
