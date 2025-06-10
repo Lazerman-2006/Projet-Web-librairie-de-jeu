fb08cac9315702f98c43db1a330735fbfa159c0e
fago0014 <amelien.fagot-naude@etudiant.univ-reims.fr>
2025-06-10 14:08:23 +0200

    Add GameCategoryCollection(findGameByCategoryId) method

--- src/Entity/Collection/GameCategoryCollection.php
+++ src/Entity/Collection/GameCategoryCollection.php
@@ -11,6 +11,20 @@ use PDO;
 class GameCategoryCollection
 {
 
+    public static function findGameByCategoryId(int $categorieId): array
+    {
+        $stmt = MyPdo::getInstance()->prepare(
+            <<<'SQL'
+            SELECT g.*
+            FROM game g
+            INNER JOIN game_category cat
+            ON g.id = cat.categoryId
+            WHERE cat.categoryId = :categorieId
+            SQL
+        );
+        $stmt->execute(['categorieId' => $categorieId]);
 
+        return $stmt->fetchAll(PDO::FETCH_CLASS, Game::class);
+    }
 
 }
