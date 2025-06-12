deff23005d6fb087aad36bffd544afc148621929
fago0014 <amelien.fagot-naude@etudiant.univ-reims.fr>
2025-06-12 11:37:28 +0200

    Change in categorie.php

--- public/categorie.php
+++ public/categorie.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Entity\AppWebPage;
 use Entity\Collection\GameCategoryCollection;
+use Entity\Poster;
 
 $webpage = new AppWebPage("Jeux vidéos : la catégorie");
 
@@ -11,15 +12,22 @@ $categorieId = (int) $_GET['categorieId'];
 
 $game = GameCategoryCollection::findGameByCategoryId($categorieId);
 
+$webpage->appendContent("<div class = category_game>");
+
 foreach ($game as $each) {
+    $poster = Poster::findById($each->getPosterId());
+    $jpeg = $poster->getJpeg();
+    $base64 = base64_encode($jpeg);
+    $image = '<img src="data:image/jpeg;base64,' . $base64 . '" alt="Poster">';
     $id = $each->getId();
     $name = $each->getName();
     $year = $each->getReleaseYear();
-    $webpage->appendContent("<p> <a href=\"game.php?gameId=$id\">$name</a></p>");
+    $webpage->appendContent("<p>{$image} <a href=\"game.php?gameId=$id\">$name $year</a></p>");
 }
+$webpage->appendContent("</div>");
 
 # Obtenir le genre dans le titre de la page
-$genre = \Entity\Categorie::findDescById($categorieId);
-$webpage->setTitle("Jeux vidéos : $genre");
+$category = \Entity\Categorie::findDescById($categorieId);
+$webpage->setTitle("Jeux vidéos : $category");
 
 echo $webpage->toHTML();
