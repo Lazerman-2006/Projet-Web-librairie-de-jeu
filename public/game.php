1dff62cf7433aea2b69a1cb6d9a7dc076274baba
fago0014 <amelien.fagot-naude@etudiant.univ-reims.fr>
2025-06-13 14:49:42 +0200

    Change documentation in game.php

--- public/game.php
+++ public/game.php
@@ -10,44 +10,50 @@ use Entity\Collection\GenderCollection;
 use Entity\GameGenreCollection;
 use Entity\Poster;
 
+//Création de la page d'un jeux
 $webpage = new AppWebPage();
 
+//Vérification du type de la variable 'gameId'
 $gameId = isset($_GET['gameId']) ? (int)$_GET['gameId'] : null;
 if ($gameId === null || $gameId <= 0) {
     die("Genre ID invalide.");
 }
 
+//Création du bouton de supression
 $games = GameCollection::findByGameId($gameId);
+//Récupère l'i d'un jeu pour le formulaire de supression
 foreach ($games as $game) {
     $id = $game->getId();
-
+    //Création du bouton de supression
     $webpage->appendContent("<div class='game_page'>");
     $webpage->appendContent("<div class='buton'>");
     $name = $webpage->escapeString($game->getName());
     $webpage->setTitle("Jeux vidéos : $name");
-
     $webpage->appendContent("<a href=\"Form/game_delete.php?gameId=$gameId\">Supprimer</a>");
+
     $webpage->appendContent("</div>");
+
     $webpage->appendContent("<div class = box_3>");
 
     $webpage->appendContent("<div class = box_1>");
 
     $webpage->appendContent("<div class = 'image'>");
 
+    //Récupération du poster du jeu
     $posterId = $game->getPosterId();
     if ($posterId !== null) {
         $poster = Poster::findById($posterId);
         if ($poster !== null) {
+            //Conversion du poster pour pouvoir l'afficher
             $jpeg = $poster->getJpeg();
             $base64 = base64_encode($jpeg);
             $image = '<img src="data:image/jpeg;base64,' . $base64 . '" alt="Poster">';
             $webpage->appendContent($image);
         }
     }
-    // Sinon on n'affiche rien (pas d'image)
-
     $webpage->appendContent("</div>");
 
+    //Vérification et affichage des logo des systèmes compatible du jeu
     $webpage->appendContent("<div class='platform'>");
     $icons = '';
 
@@ -61,10 +67,14 @@ foreach ($games as $game) {
         $icons .= '<img src="svg/apple.svg" alt="Mac" style="width: 40px;">';
     }
     $webpage->appendContent("<div><p>Plaform: </p>$icons</div>");
+
+    //Affichage de l'année de sortie du jeu
     $webpage->appendContent("<p>Année de sortie: {$game->getReleaseYear()}</p>");
     $webpage->appendContent("</div>");
 
+    //Récupération de l'id du déveuloppeur
     $developerId = $game->getDeveloperId();
+    //Condition d'affichage si il n'y a pas de déveuloppeur
     if ($developerId !== null) {
         $dev = \Entity\Developer::findById($developerId);
         if ($dev) {
@@ -84,9 +94,13 @@ foreach ($games as $game) {
     $webpage->appendContent("<div class = data>");
 
 
+    //Affichage du prix du jeu
     $price = $game->getPrice() / 100;
+
     $webpage->appendContent("<div><p>Prix : {$price}€</p></div>");
 
+
+    //Affichage de la note métacritique si il y a une note métacritique
     if ($game->getMetacritic() !== null) {
         $webpage->appendContent("<div><p>Note: {$game->getMetacritic()}</p></div>");
     } else {
@@ -96,13 +110,22 @@ foreach ($games as $game) {
     $webpage->appendContent("</div>");
 
 
-
+    //Affiche la déscription d'un jeu
     $webpage->appendContent("<div class = 'desc'><p>{$game->getShortDescription()}</p></div>\n");
+
     $webpage->appendContent("</div>");
+
     $webpage->appendContent("</div>");
+
+    //Création de l'affichage des catégories du jeu
     $webpage->appendContent("<div class = game_category>");
+
+    //Récupération des catégories à partir de l'id du jeu
     $game = GameCategoryCollection::findCategoryIdByGameId($gameId);
+
     $webpage->appendContent("<div class = category_name><p>Categorie: </p></div>");
+
+    //Boucle de création des balises de chaque catégories
     foreach ($game as $gamesCat) {
         $id = $gamesCat->getId();
         $name = $webpage->escapeString($gamesCat->getDescription());
@@ -111,10 +134,15 @@ foreach ($games as $game) {
     }
     $webpage->appendContent("</div>");
 
-    // Affichage genres
+    //Création de l'affichage des genres du jeu
     $webpage->appendContent("<div class='game_genre'>");
+
     $webpage->appendContent("<div class='genre_name'><p>Genres: </p></div>");
+
+    //Récupération des genres à partir de l'id du jeu
     $genres = GameGenreCollection::findGenreIdByGameId($gameId);
+
+    //Boucle de création des balises de chaque genre
     foreach ($genres as $genre) {
         $genreId = $genre->getId();
         $genreName = $webpage->escapeString($genre->getDescription());
@@ -122,9 +150,13 @@ foreach ($games as $game) {
     }
     $webpage->appendContent("</div>");
 
+    //Bouton de retour à l'accueil
     $webpage->appendContent('<div class="bouton_game">');
-    $webpage->appendContent('<a href="index.php">Retour à laccueil</a>');
+
+    $webpage->appendContent("<a href='index.php'>Retour à l'accueil</a>");
+
     $webpage->appendContent('</div>');
+
     $webpage->appendContent("</div>");
 }
 
